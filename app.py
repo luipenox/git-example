300adbc665379f38fa862f44088ffb536ce61b6b
Luděk Reif <luipenox@gmail.com>
2025-11-08 16:30:02 +0100

    add app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from report import get_whole_data
+
+st.title('Moje webová prezentace')
+
+st.info('Velmi důležitá informace')
+
+# Generování ukázkových dat pro bar chart
+data_all = pd.DataFrame({
+    'Měsíc': ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen'],
+    'Prodeje': [120, 150, 180, 140, 200, 170],
+    'Náklady': [80, 90, 100, 85, 110, 95]
+})
+
+st.bar_chart(data_all.set_index('Měsíc'))
